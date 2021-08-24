beb9c874a145a7814b0ded5880778e1a17a9246f
HenrykMichalicki <henrykmandv@gmail.com>
2021-08-24 11:46:34 -0300

    Criar os primeiros componentes do blogPessoal via Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] //faz o link com o css para estilização da pagina//
 })
 export class AppComponent {
   title = 'BlogPessoal';
 }
+
+
